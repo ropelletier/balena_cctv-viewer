a6840d556a7a784a067122cb87e3739cafe5f534
Rob Pelletier <ropelletier@rsu87.org>
2024-02-04 19:16:30 -0500

    prod

--- cctv-viewer/entry.sh
+++ cctv-viewer/entry.sh
@@ -10,9 +10,5 @@ chmod +x /$GIT_REPO/$RUN_FILE.sh
 
 while [ ! -e /tmp/.X11-unix/X${DISPLAY#*:} ]; do sleep 0.5; done
 
-echo ${DISPLAY#*:}
-
-x11vnc -display :1 -forever -nopw &
-
 bash /$GIT_REPO/$RUN_FILE
 
